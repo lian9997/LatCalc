3824a008bd8dbaf7c1cc490f6fdd20af06f77866
leo <qq279@10.105.249.92>
2018-03-13 11:39:46 -0400

    testing

--- model/Driver.java
+++ model/Driver.java
@@ -7,6 +7,7 @@ public class Driver {
 		while(true) {
 			try {
 				System.out.printf("%f\n", test.multiSimpleBlock(in.nextLine()));
+				int a = 2;
 			} catch (UnsupportException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
