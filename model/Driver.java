966463d4b1c406a8610455d8ce92ae51e5556867
leo <https://github.com/qq279699450>
2018-03-13 12:05:05 -0400

    testing git

--- model/Driver.java
+++ model/Driver.java
@@ -7,7 +7,7 @@ public class Driver {
 		while(true) {
 			try {
 				System.out.printf("%f\n", test.multiSimpleBlock(in.nextLine()));
-				int a = 3;
+				int a = 1;
 			} catch (UnsupportException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
