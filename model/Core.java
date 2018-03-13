442b7aa532571280a11f5cbcd50684f6797f3957
leo <qq279@10.105.249.92>
2018-03-13 11:00:45 -0400

    working on power

--- model/Core.java
+++ model/Core.java
@@ -3,39 +3,39 @@ package model;
 import java.util.Arrays;
 
 public class Core {
-	
+
 	public double basicOperation(double a, double b, char operator) throws UnsupportException {
 		switch (operator) {
-			case '+':
-				return a + b;
-			case '-':
-				return a - b;
-			case '*':
-				return a * b;
-			case '/':
-				return a / b;
-			default:
-				throw new UnsupportException();
+		case '+':
+			return a + b;
+		case '-':
+			return a - b;
+		case '*':
+			return a * b;
+		case '/':
+			return a / b;
+		default:
+			throw new UnsupportException();
 		}
 	}
-	
+
 	public double simpleBlock(String data) throws UnsupportException {
 		String arguments[] = new String[3];
 		data = data.trim();
 		int charFound = 0;
 		charFound = data.indexOf('+');
-		if(charFound == -1) {
+		if (charFound == -1) {
 			charFound = data.indexOf('-');
-			if(charFound == -1) {
+			if (charFound == -1) {
 				charFound = data.indexOf('*');
-				if(charFound == -1) {
+				if (charFound == -1) {
 					data.indexOf('/');
 				}
 			}
 		}
-		if(charFound == -1) {
+		if (charFound == -1) {
 			throw new UnsupportException();
-		}else {
+		} else {
 			arguments[0] = data.substring(0, charFound).trim();
 			char operator = data.charAt(charFound);
 			arguments[1] = Character.toString(operator).trim();
@@ -45,25 +45,47 @@ public class Core {
 			return this.basicOperation(a, b, operator);
 		}
 	}
-	
 
+	public String power(String data) throws UnsupportException {
+		int powerOperator = data.indexOf('^');
+		int before = -1, after = -1;
+		if(powerOperator == -1) {
+			return data;
+		}
+		while(data.charAt(powerOperator - 1) == ')') {
+			before = data.lastIndexOf('(', powerOperator);
+			after = data.indexOf(')', before);
+			String tmp = data.substring(before, after + 1);
+			String tmpResult = this.process(tmp);
+			data = data.replace(tmp, tmpResult);
+		}
+		while(data.charAt(powerOperator + 1) == '(') {
+			after = data.indexOf(')', powerOperator);
+			before = data.lastIndexOf('(', after);
+			
+		}
+		return "0";
+	}
 	public double multiSimpleBlock(String data) throws UnsupportException {
 		String tmp;
 		data = data.trim();
 		double result;
-		int begin = data.indexOf('(');
-		int end = -1;
-		if(begin != -1) {
-			end = data.indexOf(')');
+		int begin = -1;
+		int end = data.indexOf(')');
+		if(data.matches("[0-9]+")) {
+			return Double.parseDouble(data);
 		}
-		if(data.matches("([0-9]+[.]?[0-9]*)([ ]*)(.{1})([ ]*)([0-9]+[.]?[0-9]*)")) {
+		if (end != -1) {
+			begin = data.lastIndexOf('(', end - 1);
+		}
+		if (data.matches("([0-9]+[.]?[0-9]*)([ ]*)(.{1})([ ]*)([0-9]+[.]?[0-9]*)")) {
 			return this.simpleBlock(data);
-		}else if(begin != -1) {
+		} else if (begin != -1) {
 			tmp = data.substring(begin + 1, end);
 			result = this.multiSimpleBlock(tmp);
 			data = data.replace("(" + tmp + ")", Double.toString(result));
 			return this.multiSimpleBlock(data);
-		}else {
+		} else {
 			int multiplication = data.indexOf('*');
 			int division = data.indexOf('/');
 			int addition = data.indexOf('+');
@@ -71,73 +93,85 @@ public class Core {
 			int firstOperator = -1;
 			int before = -1, after = -1;
 
-			if(multiplication != -1) {
-				if(division != -1) {
+			if (multiplication != -1) {
+				if (division != -1) {
 					firstOperator = Math.min(multiplication, division);
-				}else {
+				} else {
 					firstOperator = multiplication;
 				}
-			}else if(division != -1) {
+			} else if (division != -1) {
 				firstOperator = division;
-			}else {
+			} else {
 				firstOperator = Math.min(addition, subtraction);
 			}
 
-			int multiplicationBefore = data.lastIndexOf('*', firstOperator - 1);
-			int divisionBefore = data.lastIndexOf('/', firstOperator - 1);
-			int additionBefore = data.lastIndexOf('+', firstOperator - 1);
-			int subtractionBefore = data.lastIndexOf('-', firstOperator - 1);
-			int operatorsBefore[] = {multiplicationBefore, divisionBefore, additionBefore, subtractionBefore};
-			Arrays.sort(operatorsBefore);
-
-			for(int i = 0; i < 4; i ++) {
-				if(operatorsBefore[i] == -1) {
-					continue;
-				}else {
-					before = operatorsBefore[i];
-					break;
-				}
-			}
-
-
-			int multiplicationAfter = data.indexOf('*', firstOperator + 1);
-			int divisionAfter = data.indexOf('/', firstOperator + 1);
-			int additionAfter = data.indexOf('+', firstOperator + 1);
-			int subtractionAfter = data.indexOf('-', firstOperator + 1);
-			int operatorsAfter[] = {multiplicationAfter, divisionAfter, additionAfter, subtractionAfter};
-			Arrays.sort(operatorsAfter);
-
-			for(int i = 0; i < 4; i ++) {
-				if(operatorsAfter[i] == -1) {
-					continue;
-				}else {
-					after = operatorsAfter[i];
-					break;
-				}
-			}
-			if(before == -1) {
+			before = this.operatorBefore(data, firstOperator - 1);
+			after = this.operatorAfter(data, firstOperator + 1);
+			if (before == -1) {
 				tmp = data.substring(0, after);
-			}else if(after == -1) {
+			} else if (after == -1) {
 				tmp = data.substring(before + 1, data.length());
-			}else {
+			} else {
 				tmp = data.substring(before + 1, after);
 			}
 			result = this.multiSimpleBlock(tmp);
 			data = data.replace(tmp, Double.toString(result));
 			return this.multiSimpleBlock(data);
-			/*if(multiplication != -1) {
-				if(division == -1 || multiplication < division) {
-					if(addition == -1 || multiplication < addition) {
-						if(subtraction == -1 || multiplication < subtraction) {
-							int nextOp
-						}
-					}
-				}
-			}*/
+			/*
+			 * if(multiplication != -1) { if(division == -1 || multiplication < division) {
+			 * if(addition == -1 || multiplication < addition) { if(subtraction == -1 ||
+			 * multiplication < subtraction) { int nextOp } } } }
+			 */
 		}
 	}
-	public double process(String data) throws UnsupportException {
-		throw new UnsupportException();
+
+	public String process(String data)throws UnsupportException {
+		double result = 0;
+		try {
+			if(data.indexOf('^') != -1) {
+				data = this.power(data);
+			}else {
+				result = this.multiSimpleBlock(data);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return Double.toString(result);
 	}
 
+
+	private int operatorAfter(String data, int pos) {
+		int multiplicationAfter = data.indexOf('*', pos);
+		int divisionAfter = data.indexOf('/', pos);
+		int additionAfter = data.indexOf('+', pos);
+		int subtractionAfter = data.indexOf('-', pos);
+		int operatorsAfter[] = {multiplicationAfter, divisionAfter, additionAfter, subtractionAfter};
+		Arrays.sort(operatorsAfter);
+
+		for(int i = 0; i < 4; i ++) {
+			if(operatorsAfter[i] == -1) {
+				continue;
+			}else {
+				return operatorsAfter[i];
+			}
+		}
+		return -1;
+		}
+	private int operatorBefore(String data, int pos) {
+			int multiplicationBefore = data.lastIndexOf('*', pos);
+			int divisionBefore = data.lastIndexOf('/', pos);
+			int additionBefore = data.lastIndexOf('+', pos);
+			int subtractionBefore = data.lastIndexOf('-', pos);
+			int operatorsBefore[] = { multiplicationBefore, divisionBefore, additionBefore, subtractionBefore };
+			Arrays.sort(operatorsBefore);
+
+			for (int i = 0; i < 4; i++) {
+				if (operatorsBefore[i] == -1) {
+					continue;
+				} else {
+					return operatorsBefore[i];
+				}
+			}
+			return -1;
+	}
 }
--- model/Driver.java
+++ model/Driver.java
@@ -4,13 +4,13 @@ public class Driver {
 	public static void main(String args[]) {
 		Core test = new Core();
 		Scanner in = new Scanner(System.in);
-		//while(true) {
+		while(true) {
 			try {
-				System.out.printf("%f\n", test.multiSimpleBlock("(4 +5)*(5+5)*5-6*2"));
+				System.out.printf("%f\n", test.multiSimpleBlock(in.nextLine()));
 			} catch (UnsupportException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		//}
+		}
 	}
 }
